bc340498a0f00463dbcd06cff24895bcbb1e57a7
Marcelo <si.marcelosantos@gmail.com>
2021-12-20 14:17:26 -0300

    Adicionando Repositório C#

---
+++ CsharpBasico/CsharpBasico/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+namespace CsharpBasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Marcelo";
+            sbyte idade = 29;
+            char sexo = 'M';
+            double saldo = 12.542;
+
+            Console.WriteLine("Olá, mundo!");
+            
+            Console.WriteLine("Nome: "+nome);
+            Console.WriteLine("Idade: "+idade);
+            Console.WriteLine("Sexo: "+sexo);
+            Console.WriteLine("Saldo: "+saldo);
+            Console.WriteLine("Saldo com duas casas decimais:  "+saldo.ToString("F2"));
+            Console.WriteLine("Saldo com ponto: "+saldo.ToString(CultureInfo.InvariantCulture));
+            
+
+
+        }
+
+    }
+}
